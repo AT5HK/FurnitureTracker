b516cd30c98b52956fde4d5bdafa189da5a8fec2
Auston <auston.salvana@live.com>
2015-07-24 11:21:42 -0700

    Initial Commit

---
+++ FurnitureTracker/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  FurnitureTracker
+//
+//  Created by Auston Salvana on 7/24/15.
+//  Copyright (c) 2015 ASolo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
